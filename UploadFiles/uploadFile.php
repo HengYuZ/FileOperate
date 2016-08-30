a049d277389ece771a63c704a4e005fd1161b3e8
HengYuZ <zhanghengyu@yaochufa.com>
2016-08-30 11:48:43 +0800

    在单文件上传功能的基础上完成了多文件的上传功能。

--- UploadFiles/uploadFile.php
+++ UploadFiles/uploadFile.php
@@ -15,74 +15,129 @@ class uploadFile
     private $path;
     //上传文件大小
     private $fileSize;
-    //上传文件个数
-    private $fileNum;
     //保存的文件名
     public $saveFileName;
 
+    //设置上传规则
     public function setUploadRules($allowType, $path, $fileSize)
     {
         foreach ($allowType as $type)
             $this->allowType[] = $type;
         $this->path = $path;
-        $this->fileSize = $path;
         $this->fileSize = $fileSize;
-        //$this->fileNum = $fileNum;
     }
 
     //上传错误信息反馈方法
-    public function uploadMessage($errorCode)
+    public function uploadErrorMsg($errorCode)
     {
         echo "上传错误,";
         switch ($errorCode) {
             case 1:
-                die('上传文件大小超出了PHP设置文件的约定值');
+                echo '上传文件大小超出了PHP设置文件的约定值';
+                break;
             case 2:
-                die('上传文件大小超出了表单中的约定值');
+                echo '上传文件大小超出了表单中的约定值';
+                break;
             case 3:
-                die('文件只有部分上传了');
+                echo '文件只有部分上传了';
+                break;
             case 4:
-                die('没有上传文件');
+                echo '上传文件大小超出了表单中的约定值';
+                break;
             default:
-                die('未知错误');
+                echo '未知错误';
+        }
+    }
+
+    //获取文件的后缀名
+    public function getSuffix($file)
+    {
+        $fileInfoArray = pathinfo($file);
+        $suffix = strtolower($fileInfoArray["extension"]);
+        if (is_string($suffix))
+            return $suffix;
+        else return false;
+    }
+
+    //上传处理操作
+    public function uploadHandle($file, $index)
+    {
+        if ($file['name'][$index] == null) {
+            return false;
+        } else {
+            $errorCode = $file['error'][$index];
+            if ($errorCode > 0) {
+                $this->uploadErrorMsg($errorCode);
+                return false;
+            } elseif ($errorCode == 0) {
+                $suffix = $this->getSuffix($file['name'][$index]);
+                if (!in_array($suffix, $this->allowType)) {
+                    echo '后缀为' . "$suffix" . '是不允许上传的文件类型';
+                    return false;
+                }
+                if ($file['size'][$index] > $this->fileSize) {
+                    echo '文件大小超过' . "$this->fileSize" . '了';
+                    return false;
+                }
+                //防止上传文件被同名覆盖掉
+                $this->saveFileName = date("Y") . date("m") . date("d") . date("H") . date("i") . date("s") . rand(100, 999) . "." . $suffix;
+                //判断是否是上传文件
+                if (is_uploaded_file($file['tmp_name'][$index])) {
+                    if (!move_uploaded_file($file['tmp_name'][$index], $this->path . '/' . $this->saveFileName))
+                        echo '问题：' . $file['name'][$index] . '不能移动到指定上传目录';
+                } else
+                    return false;
+                return true;
+            }
+            return false;
         }
     }
 
     //上传结果
     public function uploadResult()
     {
-        $errorCode = $_FILES['file']['error'];
-        if ($errorCode > 0)
-            $this->uploadMessage($errorCode);
-        else if ($errorCode == 0) {
-            $parts = explode('.', $_FILES['file']['name']);
-            $suffix = end($parts);
-            if (!in_array($suffix, $this->allowType)) {
-                die("后缀为{$suffix}的文件不允许上传");
-            }
+        //存储文件上传的情况
+        $uploadStatus = array();
 
-            if ($_FILES['file']['size'] > $this->fileSize) {
-                echo $this->fileSize;
-                die("文件超过{$this->fileSize}了");
-            }
-            //防止上传文件同名被覆盖掉
-            $this->saveFileName = date("Y") . date("m") . date("d") . date("H") . date("i") . date("s") . rand(100, 999) . "." . $suffix;
-            //判断是否是上传文件
-            if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-                if (!move_uploaded_file($_FILES['file']['tmp_name'], $this->path . '/' . $this->saveFileName))
-                    die('问题：不能将文件移动到指定目录');
-            } else
-                die("问题：上传文件{$_FILES['file']['name']}不是合法文件");
-            return true;
+        foreach ($_FILES['file']['name'] as $key => $file) {
+            $result = $this->uploadHandle($_FILES['file'], $key);
+            if ($_FILES['file']['name'][$key] == NULL)
+                $this->saveFileName = '';
+            $uploadStatus[] = array(
+                'filename' => $_FILES['file']['name'][$key],
+                'saveFileName' => $this->saveFileName,
+                'result' => $result
+            );
+        }
+        $this->uploadSuccessMsg($uploadStatus);
+    }
+
+    //返回上传状态信息
+    public function uploadSuccessMsg($uploadStatus)
+    {
+        $count = 0;
+        foreach ($uploadStatus as $v) {
+            if ($v['filename'] != NULL)
+                $count++;
+        }
+        echo '上传了' . $count . '个文件<br/>';
+        foreach ($uploadStatus as $key => $status) {
+            if ($status['filename'] && $status['result'])
+                echo $status['filename'] . ' 文件保存路径: ' . $this->path . '，保存文件名为:' . $status['saveFileName'] . '   上传状态：成功<br/>';
+            elseif ($status['filename'] != NULL && $status['result'] == false)
+                echo $status['filename'] . '上传状态：失败<br/>';
         }
     }
 }
 
 date_default_timezone_set('UTC');
-$uploadfile = new uploadFile();
+$uploadFileObj = new uploadFile();
+
 //允许上传的文件格式
 $arr = array("gif", "png", "jpg", "txt");
+
 //上传文件设定保存在当前的目录下的upload文件夹下，上传文件最大为2M
-$uploadfile->setUploadRules($arr, './upload', 2000000);
-if ($uploadfile->uploadResult())
-    echo "文件上传成功，文件" . $_FILES['file']['name'] . "保存在upload文件夹下,名字为$uploadfile->saveFileName";
+$uploadFileObj->setUploadRules($arr, './upload', 2000000);
+
+$uploadFileObj->uploadResult();
+
